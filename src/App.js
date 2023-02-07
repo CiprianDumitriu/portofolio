2af10c4a60afee65ee66357296c29f5bcb57455c
CiprianDumitriu <115021619+CiprianDumitriu@users.noreply.github.com>
2023-02-07 20:20:02 +0200

    9. React Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import AboutUs from "./pages/AboutUs"
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+// Router
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />}/>
+        <Route path="/aboutus" exact element={<AboutUs />}/>
+        <Route path="/ourwork" exact element={<OurWork />}/>
+        <Route path="/contactus" exact element={<ContactUs />}/>
+      </Routes>
+      
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
         <StyledNav>
-            <h1><a id="logo" href="#">Capture</a></h1>
+            <h1><Link  id="logo" to="/" >Capture</Link ></h1>
             <ul>
                 <li>
-                    <a  href="#">1. About Us</a>
+                    <Link  href="#" to="/aboutus">1. About Us</Link >
                 </li>
                 <li>
-                    <a href="#">2. Our Work</a>
+                    <Link  href="#" to="/ourwork">2. Our Work</Link >
                 </li>
                 <li>
-                    <a href="#">3. Contact Us</a>
+                    <Link  href="#" to="/contactus">3. Contact Us</Link >
                 </li>
             </ul>
         </StyledNav>
