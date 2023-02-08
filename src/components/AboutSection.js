ad5aaa4df1f8ac0cc1824dd8bef0c4fb5bab891b
CiprianDumitriu <115021619+CiprianDumitriu@users.noreply.github.com>
2023-02-08 19:08:16 +0200

    14. About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,30 +5,13 @@ import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
-
-    const titleAnim = {
-        hidden: {opacity: 0},
-        show: {opacity: 1, transition: {duration: 2}},
-    }
-    const container = {
-        hidden: {x:100},
-        show: {
-            x:0, 
-            transition: {
-                duration:1,
-                ease: "easeOut",
-                staggerChildren: 1,
-                when: "afterChildren",
-            }
-        },
-    }
-
     return(
         <About>
             <Description>
-                <motion.div variants={container} initial="hidden" animate="show" className="title">
+                <motion.div >
                     <Hide>
                         <motion.h2 variants={titleAnim} >We work to make</motion.h2>
                     </Hide>
@@ -39,11 +22,11 @@ const AboutSection = () => {
                         <motion.h2 variants={titleAnim} >true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera" />
+                <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
             </Image>
         </About>
     )
