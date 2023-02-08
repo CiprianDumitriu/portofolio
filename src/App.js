13bae7b47217cbce72a6db7b9d7297f9ce8e4f0e
CiprianDumitriu <115021619+CiprianDumitriu@users.noreply.github.com>
2023-02-08 18:24:54 +0200

    13. Page Transitions

--- src/App.js
+++ src/App.js
@@ -8,20 +8,25 @@ import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
 import MovieDetail from "./pages/MovieDetails";
 // Router
-import {Routes, Route, Outlet} from "react-router-dom";
+import {Routes, Route, Outlet, useLocation} from "react-router-dom";
+// Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Routes>
-        <Route path="/" exact element={<AboutUs />}/>
-        <Route path="/aboutus" exact element={<AboutUs />}/>
-        <Route path="/ourwork" exact element={<OurWork />}/>
-        <Route path="/ourwork/:id"  element={<MovieDetail />}/>
-        <Route path="/contactus" exact element={<ContactUs />}/>
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" exact element={<AboutUs />}/>
+          <Route path="/aboutus" exact element={<AboutUs />}/>
+          <Route path="/ourwork" exact element={<OurWork />}/>
+          <Route path="/ourwork/:id"  element={<MovieDetail />}/>
+          <Route path="/contactus" exact element={<ContactUs />}/>
+        </Routes>
+      </AnimatePresence>
       <Outlet />
     </div>
   );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import { MovieState } from "../movieState";
+// Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 
 const MovieDetail = () => {
@@ -20,7 +23,7 @@ const MovieDetail = () => {
     return (
         <>
         {movie && (
-            <Details>
+            <Details variants={pageAnimation} initial="hidden" animate="show" exit="exit">
                 <Headline>
                     <h2>{movie.title}</h2>
                     <img src={movie.mainImg} alt="movie" />
@@ -39,7 +42,7 @@ const MovieDetail = () => {
     )
 }
 
-const Details = styled.div`
+const Details = styled(motion.div)`
     color: white;
 `
 const Headline = styled.div`
